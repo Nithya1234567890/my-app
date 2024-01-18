5affe10da0990169d7a8c85acf1906138f2dd65a
Nithya1234567890 <nithyamunagapati@gmai.com>
2024-01-18 21:38:11 +0530

    Updates

---
+++ precache-manifest.4a06569658993b9030717d14d5c9a5e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c1eeadffd705d6ec37d75efec2b77b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "4198d2b523f28f882e91",
+    "url": "/my-app/static/css/main.208abf41.chunk.css"
+  },
+  {
+    "revision": "7fac68e8b9cfeb9ea334",
+    "url": "/my-app/static/js/2.e6eb7605.chunk.js"
+  },
+  {
+    "revision": "a68fb9a705c058f3aaed8f74c7821cda",
+    "url": "/my-app/static/js/2.e6eb7605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a305264669ab0698eb9",
+    "url": "/my-app/static/js/3.a8bd2e92.chunk.js"
+  },
+  {
+    "revision": "4198d2b523f28f882e91",
+    "url": "/my-app/static/js/main.32c3143d.chunk.js"
+  },
+  {
+    "revision": "268e9915b5cad5b112be",
+    "url": "/my-app/static/js/runtime-main.245f70bb.js"
+  }
+]);
